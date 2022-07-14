659bd8ca8049ad879428b034a7f2c7ab5c896186
leoamsbb <mrunalbadhe24@gmail.com>
2022-07-14 10:55:29 +0530

    217. Contains Duplicate : Attempt 2

--- src/main/scala/ContainsDuplicate_217.scala
+++ src/main/scala/ContainsDuplicate_217.scala
@@ -2,26 +2,41 @@
 
 // https://leetcode.com/problems/contains-duplicate/
 object ContainsDuplicate_217 {
-  import scala.annotation.tailrec
+
+  import scala.collection.mutable
 
   def containsDuplicate(nums: Array[Int]): Boolean = {
 
-    @tailrec
-    def loop(head: Int, tail: Array[Int], acc: Map[Int, Boolean]): Boolean = {
-      if (acc.contains(head))
-        true
-      else if (tail.isEmpty)
-        false
-      else
-        loop (tail.head, tail.tail, acc + (head -> true))
+    var left = 0
+    var right = nums.length - 1
+    val map = mutable.Map.empty[Int, Boolean]
+
+    while (left <= right) {
+      if (left == right)
+        return map.contains(nums(left))
+
+      val l = nums(left)
+      val r = nums(right)
+      if (l == r)
+        return true
+      else if (map.contains(l) || map.contains(r))
+        return true
+      else {
+        map.put(l, true)
+        map.put(r, true)
+      }
+
+      left += 1
+      right -= 1
     }
 
-    loop (nums.head, nums.tail, Map())
+    false
   }
 
   def main(args: Array[String]): Unit = {
     assert(containsDuplicate(Array(1, 2, 3, 1)))
     assert(!containsDuplicate(Array(1, 2, 3, 4)))
-    assert(containsDuplicate(Array(1,1,1,3,3,4,3,2,4,2)))
+    assert(containsDuplicate(Array(1, 1, 1, 3, 3, 4, 3, 2, 4, 2)))
+    assert(containsDuplicate(Array(1000000000, 1000000000, 11)))
   }
 }
