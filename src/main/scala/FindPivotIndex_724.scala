d7c65672f4df6bb0a3c44113001b245c42a8bb0b
leoamsbb <mrunalbadhe24@gmail.com>
2022-07-15 21:59:09 +0530

    724. Find Pivot Index : Still slow but works

--- src/main/scala/FindPivotIndex_724.scala
+++ src/main/scala/FindPivotIndex_724.scala
@@ -2,7 +2,7 @@
 // https://leetcode.com/problems/find-pivot-index/
 object FindPivotIndex_724 {
 
-  def pivotIndex(nums: Array[Int]): Int = {
+  def pivotIndex_doesntWorkForNegativeArrays(nums: Array[Int]): Int = {
     var mid = nums.length / 2
     val (left, right) = nums.splitAt(mid)
     var leftSum = left.sum
@@ -31,7 +31,38 @@ object FindPivotIndex_724 {
     if (leftSum == rightSum) mid else -1
   }
 
+  import scala.annotation.tailrec
+
+  // Using Hint
+  def pivotIndex(nums: Array[Int]): Int = {
+
+    val prefixSum = nums.foldLeft(List.empty[Int]) {
+      case (Nil, elem) => elem :: Nil
+      case (acc, elem) => acc.head + elem :: acc
+    }.reverse
+
+    val lastIndex = nums.length - 1
+
+    @tailrec
+    def loop(index: Int): Int = {
+      if (index < 0 || index > lastIndex)
+        return -1
+
+      val left = if (index == 0) 0 else prefixSum(index - 1)
+      val right = if(index == lastIndex) 0 else prefixSum(lastIndex) - prefixSum(index)
+
+      if (left == right)
+        index
+      else
+        loop(index + 1)
+    }
+
+    loop(0)
+  }
+
   def main(args: Array[String]): Unit = {
+    assert(pivotIndex(Array(-1,-1,-1,1,1,1)) == -1)
+    assert(pivotIndex(Array(-1,-1,-1,-1,-1,0)) == 2)
     assert(pivotIndex(Array(2, 1, -1)) == 0)
     assert(pivotIndex(Array(1, -1, 4)) == 2)
     assert(pivotIndex(Array(1, 7, 3, 6, 5, 6)) == 3)
